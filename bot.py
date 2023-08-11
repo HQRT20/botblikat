16667ff0cca22686248e46e239f7bd8b1125ec30
HQRT20 <76974137+HQRT20@users.noreply.github.com>
2023-08-11 17:04:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,11 +7,11 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 API_ID = "12230620"
 API_HASH = "6a754763979dd2cf139687be3e89901d"
 BOT_TOKEN = "5765546953:AAG_UdX2moG7ryDHuERp6cXf4yiGGdu58kg"
-AUTHORIZED_USER_ID = @DirtyBand
+AUTHORIZED_USER_ID = "@DirtyBand"
 
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-authorized_user_ids = [AUTHORIZED_USER_ID]
+authorized_user_ids = [int(AUTHORIZED_USER_ID)]
 user_tokens = {}
 user_codes = {}
 
@@ -58,16 +58,6 @@ def callback_query(client, callback_query):
         callback_query.message.edit_text("ليس لديك الحق في استخدام البوت في الوقت الحالي.")
 
 @app.on_message(filters.private & ~filters.user(AUTHORIZED_USER_ID))
-def start(client, message):
-    user_id = message.from_user.id
-    user_code = message.text
-    if user_id in user_codes and user_code == user_codes[user_id]:
-        user_tokens[user_id] = time.time() + (24 * 60 * 60)  # قم بتغيير المدة حسب متطلباتك
-        message.reply_text("تم تفعيل استخدام البوت. يمكنك البدء في رفع البلاغات.")
-    else:
-        message.reply_text("ليس لديك الحق في استخدام البوت في الوقت الحالي. يرجى التحقق من الكود والمحاولة مرة أخرى.")
-
-@app.on_message(filters.private & ~filters.user(AUTHORIZED_USER_ID))
 def process_user_message(client, message):
     user_id = message.from_user.id
     if user_id in user_tokens and time.time() <= user_tokens[user_id]:
@@ -75,26 +65,27 @@ def process_user_message(client, message):
         selected_emails = ["stopCA@telegram.org", "abuse@telegram.org"]
         email_message = f"بريد المستخدم: {message.text}\nموضوع البلاغ: {message.text}\nالرسالة: {message.text}"
         
-        
+        # إرسال البلاغات إلى عناوين البريد المحددة
         
         time.sleep(5)
         inline_keyboard = [
             [InlineKeyboardButton("عدد البلاغات", callback_data="get_report_count")]
         ]
-        reply_markup =InlineKeyboardMarkup(inline_keyboard)
-        message.reply_text("تم رفع البلاغ. انقر على 'عدد البلاغات' لمعرفة عدد البلاغات التي تم رفعها.", reply_markup=reply_markup)
-    else:
-        message.reply_text("ليس لديك الحق في استخدام البوت في الوقت الحالي.")
+        reply_markup = InlineKeyboardMarkup(inline_keyboard)
+        message.reply_text("تم رفع البلاغ. انقر على 'عدد البلاغات' لمعرفة عدد البلاغات التي تم رفعها.", reply_markup=reply_markup)
+    else:
+        message.reply_text("ليس لديك الحق في استخدام البوت في الوقت الحالي.")
 
 @app.on_callback_query()
 def get_report_count(client, callback_query):
-    user_id = callback_query.from_user.id
-    if user_id in user_tokens and time.time() <= user_tokens[user_id]:
-        user_token = user_tokens[user_id]
-        report_count = 5  # قم بتغييره إلى القيمة الفعلية
-        remaining_time = user_tokens[user_id] - time.time()
-        callback_query.message.edit_text("عدد البلاغات المرفوعة: {}\nمدة الصلاحية المتبقية: {} ثواني".format(report_count, int(remaining_time)))
-    else:
-        callback_query.message.edit_text("ليس لديك الحق في استخدام البوت في الوقت الحالي.")
+    user_id = callback_query.from_user.id
+    if user_id in user_tokens and time.time() <= user_tokens[user_id]:
+        user_token = user_tokens[user_id]
+        report_count = 5  # قم بتغييره إلى القيمة الفعلية
+remaining_time = user_tokens[user_id] - time.time()
+        callback_query.message.edit_text("عدد البلاغات المرفوعة: {}\nمدة الصلاحية المتبقية: {} ثواني".format(report_count, int(remaining_time)))
+  
+  else:
+        callback_query.message.edit_text("ليس لديك الحق في استخدام البوت في الوقت الحالي.")
 
-app.run() 
+app.run()
